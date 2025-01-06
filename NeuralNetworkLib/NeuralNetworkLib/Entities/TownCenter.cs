8e8791bd5a6838b86a04f39926aef653e8e71820
BarraSantiago <barrasanti@gmail.com>
2025-01-06 16:23:46 -0300

    Update TownCenter.cs

--- NeuralNetworkLib/NeuralNetworkLib/Entities/TownCenter.cs
+++ NeuralNetworkLib/NeuralNetworkLib/Entities/TownCenter.cs
@@ -2,12 +2,21 @@
 
 namespace NeuralNetworkLib.Entities;
 
-
 public struct CreationCost
 {
     public int Gold;
     public int Wood;
     public int Food;
+
+    public CreationCost Sum(CreationCost a)
+    {
+        return new CreationCost
+        {
+            Gold = a.Gold + Gold,
+            Wood = a.Wood + Wood,
+            Food = a.Food + Food
+        };
+    }
 }
 
 public class TownCenter
@@ -17,17 +26,33 @@ public class TownCenter
     private int food;
     private int _initialCarts = 1;
     private int _initialBuilders = 1;
-    private int _initialGatherer = 1;
-    private SimNode<IVector> coordinates;
-    
-    public int Gold { get => gold; set => gold = value; }
-    public int Wood { get => wood; set => wood = value; }
-    public int Food { get => food; set => food = value; }
+    private int _initialGatherer = 5;
+    public SimNode<IVector> position;
+    int gathererCount = 5;
+
+    public int Gold
+    {
+        get => gold;
+        set => gold = value;
+    }
+
+    public int Wood
+    {
+        get => wood;
+        set => wood = value;
+    }
+
+    public int Food
+    {
+        get => food;
+        set => food = value;
+    }
+
     public CreationCost CartCost = new CreationCost { Gold = 20, Wood = 30, Food = 5 };
     public CreationCost BuilderCost = new CreationCost { Gold = 8, Wood = 15, Food = 10 };
     public CreationCost GathererCost = new CreationCost { Gold = 5, Wood = 2, Food = 10 };
     public CreationCost WatchTowerCost = new CreationCost { Gold = 200, Wood = 400, Food = 0 };
-    
+
 
     public TownCenter()
     {
@@ -35,52 +60,74 @@ public class TownCenter
         wood = 0;
         food = 0;
     }
-    
+
+    public void ManageSpawning()
+    {
+        if (Gold < GathererCost.Gold || Wood < GathererCost.Wood || Food < GathererCost.Food) return;
+        
+        if (gathererCount % 3 == 0 && !HasEnoughResources(BuilderCost.Sum(CartCost.Sum(GathererCost))))
+        {
+            return;
+        }
+
+        SpawnGatherer();
+        gathererCount++;
+
+        if (gathererCount % 3 == 0)
+        {
+            SpawnBuilder();
+            SpawnCart();
+        }
+    }
+
     public void SpawnInitialUnits()
     {
         for (int i = 0; i < _initialCarts; i++)
         {
             // Spawn Cart
         }
-        
+
         for (int i = 0; i < _initialBuilders; i++)
         {
             // Spawn Builder
         }
-        
+
         for (int i = 0; i < _initialGatherer; i++)
         {
             // Spawn Gatherer
         }
     }
-    
-    public void SetCoordinates(SimNode<IVector> coordinates)
-    {
-        this.coordinates = coordinates;
-    }
-    
+
     public void SpawnCart()
     {
+        if (!HasEnoughResources(CartCost)) return;
         ReduceResources(CartCost);
         // Spawn Cart
     }
-    
+
     public void SpawnBuilder()
     {
+        if (!HasEnoughResources(BuilderCost)) return;
         ReduceResources(BuilderCost);
         // Spawn Builder
     }
-    
+
     public void SpawnGatherer()
     {
+        if (!HasEnoughResources(GathererCost)) return;
         ReduceResources(GathererCost);
         // Spawn Gatherer
     }
-    
+
     public void ReduceResources(CreationCost cost)
     {
         gold -= cost.Gold;
         wood -= cost.Wood;
         food -= cost.Food;
+    }
+
+    public bool HasEnoughResources(CreationCost cost)
+    {
+        return gold >= cost.Gold && wood >= cost.Wood && food >= cost.Food;
     }
 }
